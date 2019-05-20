ebfa085acfb8393e79ffdb92008e7852157b82d7
Emese Mathe <emese.mathe@virtual7.ro>
2019-05-20 23:16:16 +0300

    ADDED: cleaing supply model + MODIFIED billboards model

--- lib/models/bill_board.dart
+++ lib/models/bill_board.dart
@@ -4,7 +4,8 @@ class BillBoard {
   final String descrition;
   final String type;
   final double expirationTime;
+  final String userId;
 
   BillBoard(
-      this.id, this.title, this.descrition, this.type, this.expirationTime);
+      this.id, this.title, this.descrition, this.type, this.expirationTime, this.userId);
 }
---
+++ lib/models/cleaning_suppliy.dart
@@ -0,0 +1,9 @@
+class CleainingSupply{
+  final String id;
+  final String name;
+  final String type;
+  final double price;
+  final String userId;
+
+  CleainingSupply(this.id, this.name, this.type, this.price, this.userId);
+}
