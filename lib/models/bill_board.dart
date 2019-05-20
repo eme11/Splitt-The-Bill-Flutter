2b73710f7b903f10e11255cf29dc2f9fb0dec3fe
Emese Mathe <emese.mathe@virtual7.ro>
2019-05-20 23:13:17 +0300

    ADDED: bill board model

---
+++ lib/models/bill_board.dart
@@ -0,0 +1,10 @@
+class BillBoard {
+  final String id;
+  final String title;
+  final String descrition;
+  final String type;
+  final double expirationTime;
+
+  BillBoard(
+      this.id, this.title, this.descrition, this.type, this.expirationTime);
+}
