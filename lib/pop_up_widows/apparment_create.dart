f8ed462ff612bc2ee59fda2dda906ef5a6730bad
Emese Mathe <emese.mathe@virtual7.ro>
2019-05-20 17:37:21 +0300

    ADDED: create apartment form

--- lib/pop_up_widows/apparment_create.dart
+++ lib/pop_up_widows/apparment_create.dart
@@ -1,21 +1,129 @@
 import 'package:flutter/material.dart';
 
-class ApartmentCreate extends StatefulWidget{
+class ApartmentCreate extends StatefulWidget {
+
   @override
   State<StatefulWidget> createState() {
     return _ApartmentCreateState();
   }
-
 }
 
-class _ApartmentCreateState extends State<ApartmentCreate>{
+class _ApartmentCreateState extends State<ApartmentCreate> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  List<Map<String, dynamic>> userList = [];
+  Map<String, dynamic> _address = {
+    'city': null,
+    'country': null,
+    'street': null,
+    'building': null,
+    'number': 0
+  };
 
-  Widget _buildBody(BuildContext context){
-
+  Widget _buildSizedBox() {
+    return SizedBox(
+      height: 10.0,
+    );
   }
 
-  void _submitForm(){
+  Widget _buildStreet() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Street Address', filled: true, fillColor: Colors.white),
+      validator: (String value) {
+        if (value.isEmpty || value.length < 6) {
+          return 'Invalid Street name';
+        }
+      },
+      onSaved: (String value) {
+        _address['street'] = value;
+      },
+    );
+  }
 
+  Widget _buildCountry() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Country', filled: true, fillColor: Colors.white),
+      validator: (String value) {
+        if (value.isEmpty || value.length < 6) {
+          return 'Invalid country name';
+        }
+      },
+      onSaved: (String value) {
+        _address['country'] = value;
+      },
+    );
+  }
+
+  Widget _buildCity() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'City', filled: true, fillColor: Colors.white),
+      validator: (String value) {
+        if (value.isEmpty || value.length < 6) {
+          return 'Invalid city name';
+        }
+      },
+      onSaved: (String value) {
+        _address['city'] = value;
+      },
+    );
+  }
+
+  Widget _buildNumber() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Number', filled: true, fillColor: Colors.white),
+      validator: (String value) {
+        if (value.isEmpty ||
+            !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(value)) {
+          return 'Invalid number';
+        }
+      },
+      onSaved: (String value) {
+        _address['number'] = value;
+      },
+    );
+  }
+
+  void _addCurrentUser() {
+    return userList.add({
+      'lastName': 'Mathe',
+      'firstName': 'Emese',
+      'nickName': 'Eme',
+      'number': '0740797202',
+      'email': 'emese.mathe.07@gmail.com'
+    });
+  }
+
+  Widget _buildBody(BuildContext context) {
+    _addCurrentUser();
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            _buildStreet(),
+            _buildSizedBox(),
+            _buildSizedBox(),
+            _buildNumber(),
+            _buildSizedBox(),
+            _buildCity(),
+            _buildSizedBox(),
+            _buildCountry(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _submitForm() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+
+    _formKey.currentState.save();
+    Navigator.of(context).pop();
   }
 
   @override
@@ -41,5 +149,4 @@ class _ApartmentCreateState extends State<ApartmentCreate>{
       ],
     );
   }
-
 }
