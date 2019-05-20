1f713d377bcfeac8abce5a71eeb7fa03fd29a060
Emese Mathe <emese.mathe@virtual7.ro>
2019-05-20 23:09:26 +0300

    ADDED: user model

---
+++ lib/models/user.dart
@@ -0,0 +1,11 @@
+class User {
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String phone;
+  final String nickName;
+
+  User(this.id, this.firstName, this.lastName, this.nickName, this.email,
+      this.phone);
+}
