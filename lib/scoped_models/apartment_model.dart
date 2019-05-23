141c4bc95b775bfcf2d9b0eab27159b39398ece3
Emese Mathe <emese.mathe@virtual7.ro>
2019-05-23 17:16:13 +0300

    Started refactoring apartment

--- lib/scoped_models/apartment_model.dart
+++ lib/scoped_models/apartment_model.dart
@@ -1,12 +1,38 @@
 import 'package:scoped_model/scoped_model.dart';
 
 import '../models/apartment.dart';
+import '../models/user.dart';
 
 mixin ApartmentModel on Model{
-  Apartment _currentApartment;
+  Apartment _currentApartment = Apartment('2', 'blah blah', 2, 'Timisoara', 'Romania');
+  List<User> _userList = [];
+
+  List<User> get userList{
+    return List.from(_userList);
+  }
+
+  void addUser(User user){
+    _userList.add(user);
+    notifyListeners();
+  }
+
+  void deleteUserAt(int index){
+    _userList.removeAt(index);
+    notifyListeners();
+  }
+
+  void deleteUser(User user){
+    _userList.remove(user);
+    notifyListeners();
+  }
 
   Apartment get currentApartmnet{
     return _currentApartment;
   }
+
+  void setcurrentApartment(Apartment apartment){
+    _currentApartment = apartment;
+  }
+
 
 }
