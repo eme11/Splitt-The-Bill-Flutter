a77a40b4b29f8754414e3af0b989d15fe073df6c
Emese Mathe <emese.mathe@virtual7.ro>
2019-05-23 12:21:00 +0300

    ADDED: bill board scoped model

--- lib/scoped_models/main_model.dart
+++ lib/scoped_models/main_model.dart
@@ -1,5 +1,6 @@
 import 'package:scoped_model/scoped_model.dart';
 
 import 'supply.dart';
+import 'bill_board_model.dart';
 
-class MainModel extends Model with SupplyModel{}
+class MainModel extends Model with SupplyModel, BillBoardModel {}
