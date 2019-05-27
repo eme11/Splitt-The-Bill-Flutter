d60fe08419a9e1a1569a59d90939867e93c32a75
Emese Mathe <emese.mathe@virtual7.ro>
2019-05-27 17:43:25 +0300

    ADDED: aid for user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -5,9 +5,10 @@ class User {
   String email;
   String phone;
   String nickName;
+  String aid;
 
   User(this.id, this.firstName, this.lastName, this.nickName, this.email,
-      this.phone);
+      this.phone, {this.aid});
 
   Map<String, dynamic> getUserMap(){
     final Map<String, dynamic> _user ={
@@ -16,6 +17,7 @@ class User {
       'email':email,
       'number':phone,
       'nickName':nickName,
+      'aid':aid
     };
     return _user;
   }
