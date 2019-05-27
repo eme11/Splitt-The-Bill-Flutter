da312b9ba7845f103a7c327ef571de4d1a06ba3e
Emese Mathe <emese.mathe@virtual7.ro>
2019-05-27 16:01:36 +0300

    CONNECTED: chores data to firebase

--- lib/models/chores.dart
+++ lib/models/chores.dart
@@ -1,11 +1,12 @@
 class Chore{
   String aid;
+  String id;
   String name;
   String description;
   int changingInterval;
   String currentAssigneeId;
 
-  Chore(this.name, this.description, this.changingInterval);
+  Chore(this.name, this.description, this.changingInterval, {this.id, this.aid, this.currentAssigneeId});
 
   void setCurrentAssigneeId(String uid){
     currentAssigneeId = uid;
@@ -21,6 +22,10 @@ class Chore{
 
   String get apartmentId{
     return aid;
+  }
+
+  void setId(String value){
+    id = value;
   }
 
   Map<String, dynamic> getChoreMap(){
