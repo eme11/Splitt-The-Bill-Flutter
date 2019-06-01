d655e89369eeac1ea14ed6b1782a0999d07f654d
Emese Mathe <emese.mathe@virtual7.ro>
2019-06-01 13:57:32 +0300

    MODIFIED: about page final touch up

--- lib/more_page_details/about_page.dart
+++ lib/more_page_details/about_page.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
-  Widget _buildSizedBox(){
-    return SizedBox(height: 10.0,);
+  Widget _buildSizedBox() {
+    return SizedBox(
+      height: 10.0,
+    );
   }
+
   Widget _buildBody() {
     return Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text("Bachelor Degree Project"),
-              _buildSizedBox(),
-              Text("Student: Mathe Emese"),
-              _buildSizedBox(),
-              Text("Professor: Dr. Ing. Casandra Holotescu" ),
-              _buildSizedBox(),
-              _buildSizedBox(),
-              Text('Company log'),
-              _buildSizedBox(),
-              Image.asset('assets/virtual7.png')
+          Text(
+            "Bachelor Degree Project",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          _buildSizedBox(),
+          _buildSizedBox(),
+          _buildSizedBox(),
+          Text("Student"),
+          Text(
+            "Emese MATHE",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          _buildSizedBox(),
+          Text('Supervisor'),
+          Text(
+            "Prof. dr. ing. Oana Iulia Casandra HOLOTESCU",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          _buildSizedBox(),
+          _buildSizedBox(),
+          _buildSizedBox(),
+          _buildSizedBox(),
+          Image.asset('assets/virtual7.png'),
+          _buildSizedBox(),
+          _buildSizedBox(),
+          Text('Company logo')
         ]));
   }
 
